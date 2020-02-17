8df3ad21793b806b67e668c3e9033e82f85700ec
Игорь <just4-spam@mail.ru>
2020-02-17 14:58:15 +0300

    first

---
+++ src/main/java/TrainForm.java
@@ -0,0 +1,167 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TrainForm {
+    private JTextField answerField;
+    private JButton addBut;
+    private JButton hintBut;
+    private JButton changeBut;
+    private JButton restartBut;
+    private JLabel questionField;
+    private JLabel messageField;
+    private JPanel rootPanel;
+    private JLabel languageField;
+    private JPanel backgroundPanel;
+    private JPanel ButtonPanel;
+    private JPanel TextPanel;
+    private JLabel counterLabel;
+    private static Color blue = new Color(63, 147, 145);
+    private static Manager manager = new Manager();
+    private static TrainForm trainForm = new TrainForm();
+
+
+    public TrainForm() {
+
+
+        answerField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                makeProcesing();
+            }
+        });
+
+        restartBut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                manager.createVocabulary();
+                messageField.setText("Try to translate!");
+
+                addBut.setVisible(true);
+                hintBut.setVisible(true);
+                changeBut.setVisible(true);
+                languageField.setVisible(true);
+                answerField.setVisible(true);
+                counterLabel.setVisible(true);
+
+                trainForm.start();
+            }
+        });
+
+
+        hintBut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                messageField.setText(manager.getAnswer());
+            }
+        });
+        addBut.addActionListener(new ActionListener() {
+           // @Override
+            public void actionPerformed(ActionEvent e) {
+                AddWordForm myDialog = new AddWordForm();
+                myDialog.setTitle("Cozy Train 3.0");
+                myDialog.setLocation(510, 250);
+                //myDialog.setIconImage(makeIcon());
+                myDialog.pack();
+                myDialog.setVisible(true);
+            }
+        });
+        changeBut.addActionListener(new ActionListener() {
+            //@Override
+            public void actionPerformed(ActionEvent e) {
+
+                switch (manager.getLang()) {
+
+                    case 0:
+                        manager.setLang(1);
+                        manager.setLanguage("Russian");
+                        break;
+
+                    case 1:
+                        manager.setLang(0);
+                        manager.setLanguage("English");
+                        break;
+
+                }
+
+                start();
+
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        JFrame frame = new JFrame("Cozy Train 3.0");
+        frame.setContentPane(trainForm.rootPanel);
+        //frame.setBackground(blue);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//        frame.setIconImage(makeIcon());
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setBounds(400, 200, 500, 250);
+        //frame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:/Java/Projects/CozyTrain/src/main/java/cozyTrain.ico"));
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("src/cozyTrain.png"));
+        manager.createVocabulary();
+        trainForm.start();
+
+    }
+
+    public void start() {
+        System.out.println("Start");
+        manager.createQuestion();
+        questionField.setText(manager.getQuestion());
+        answerField.setText("");
+        counterLabel.setText("Remain " + manager.getArraySize() + " words");
+        languageField.setText("Current language: " + manager.getLanguage());
+
+    }
+
+    private void makeProcesing() {
+
+        if (manager.getArraySize() == 0) {
+
+            finalState();
+
+        } else if (answerField.getText().equals(manager.getAnswer())) {
+
+            messageField.setText("Right!");
+
+            if (manager.getArraySize() == 1) {
+                manager.remooveWordFromVocabulary();
+                finalState();
+
+            } else {
+                manager.remooveWordFromVocabulary();
+                start();
+            }
+
+        } else {
+            messageField.setText("No, it's not the answer");
+            start();
+        }
+
+
+    }
+
+    public void finalState() {
+        questionField.setText("All words are tranlated!");
+        answerField.setText("");
+        messageField.setText("Congrats you smartass!");
+        languageField.setText("You are smartass!");
+        addBut.setVisible(false);
+        hintBut.setVisible(false);
+        changeBut.setVisible(false);
+        languageField.setVisible(false);
+        answerField.setVisible(false);
+        counterLabel.setVisible(false);
+
+    }
+
+}
+
+
+
---
+++ src/main/java/Word.java
@@ -0,0 +1,14 @@
+public class Word {
+
+    private String eng, rus;
+
+    Word(String eng, String rus) {
+        this.eng = eng;
+        this.rus = rus;
+    }
+
+    public String getEng() {return eng;}
+
+    public String getRus() {return rus;}
+
+}
